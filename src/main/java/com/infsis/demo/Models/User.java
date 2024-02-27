feb20dedd59351c2a8a010da6b1ea2bd298b050e
LionelCMZ <moraleslionel792@gmail.com>
2024-02-26 23:01:39 -0400

    first commit

--- src/main/java/com/infsis/demo/Models/User.java
+++ src/main/java/com/infsis/demo/Models/User.java
@@ -2,11 +2,14 @@ package com.infsis.demo.Models;
 
 import jakarta.persistence.*;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
+@Table(name = "\"user\"")
+@EntityListeners(AuditingEntityListener.class)
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
